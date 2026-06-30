bae627bd18d1e62308e39a6110fe2121bff6fa16
Megan Moody <meganboothemail@gmail.com>
2026-06-29 20:42:59 -0700

    Phase 0 Sliders - rewritten with improved variable naming and structure

--- shared/src/main/java/chess/MovementCalculator.java
+++ shared/src/main/java/chess/MovementCalculator.java
@@ -1,10 +1,44 @@
 package chess;
 
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class MovementCalculator {
+
+    public static class Sliders {
+        public static Collection<ChessMove> calculate(ChessBoard board, ChessPiece piece, ChessPosition position, int[][] directions) {
+            List<ChessMove> moves = new ArrayList<>();
+
+            for(int[] dir : directions){
+                int row = position.getRow();
+                int col = position.getColumn();
+
+                while(true){
+                    row += dir[0];
+                    col += dir[1];
+
+                    if (isOutOfBounds(row,col)) {
+                        break;
+                    }
+
+                    ChessPiece obstacle = getObstacle(board,row,col);
+                    if (isBlocked(obstacle,piece)) {
+                        break;
+                    }
+
+                    if (isCapture(obstacle, piece)) {
+                        recordMoves(moves,position,row,col);
+                        break;
+                    }
+
+                    recordMoves(moves, position, row, col);
+                }
+            }
+            return moves;
+        }
+    }
 
     private static boolean isOutOfBounds(int row, int col) {
         return row < 1 || row > 8 || col < 1 || col > 8;
@@ -28,33 +62,6 @@ public class MovementCalculator {
 
     private static void recordMoves(List<ChessMove> moves, ChessPosition from, int toRow, int toCol, ChessPiece.PieceType promotion){
         moves.add(new ChessMove(from, new ChessPosition(toRow, toCol), promotion));
-    }
-
-    public static class Sliders {
-        public static Collection<ChessMove> calculate(ChessBoard board, ChessPiece piece, ChessPosition position, int[][] directions){
-
-            List<ChessMove> moves = new ArrayList<>();
-
-            for (int[] dir : directions) {
-                int j = position.getRow();
-                int k = position.getColumn();
-
-                while (true) {
-                    j += dir[0];
-                    k += dir[1];
-
-                    if (isOutOfBounds(j,k)) {break;}
-
-                    ChessPiece obstacle = getObstacle(board,j,k);
-
-                    if (isBlocked(obstacle, piece)) {break;}
-                    if (isCapture(obstacle, piece)) {recordMoves(moves,position,j,k);break;}
-
-                    recordMoves(moves,position,j,k);
-                }
-            }
-            return moves;
-        }
     }
 
     public static class Steppers {
