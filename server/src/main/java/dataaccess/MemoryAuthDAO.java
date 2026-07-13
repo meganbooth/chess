eb5debd0d62c68bf9cc1c7e5e27f67cf80ee04f1
Megan Moody <meganboothemail@gmail.com>
2026-07-12 20:24:06 -0700

    Phase 3 create data models, DAO interfaces, and in-memory implementations for users, auth tokens, and games

---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,30 @@
+package dataaccess;
+
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryAuthDAO implements AuthDAO{
+    private final Map<String, AuthData> auths = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        auths.clear();
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        auths.remove(authToken);
+    }
+}
---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,25 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUserDAO implements UserDAO{
+    private final Map<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+}
