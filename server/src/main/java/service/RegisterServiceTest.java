884829b554492dfd85397dc1edc3f46e2048047a
Megan Moody <meganboothemail@gmail.com>
2026-07-15 16:54:20 -0700

    Phase 3 Unit Tests - implement register service test

---
+++ server/src/main/java/service/RegisterServiceTest.java
@@ -0,0 +1,35 @@
+package service;
+
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.request.RegisterRequest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RegisterServiceTest {
+    private MemoryUserDAO userDAO;
+    private MemoryAuthDAO authDAO;
+    private RegisterService registerService;
+
+    @BeforeEach
+    public void setup() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        registerService = new RegisterService(userDAO, authDAO);
+    }
+
+    @Test
+    public void registerSuccess(){
+        assertDoesNotThrow(() -> registerService.register(new RegisterRequest("username", "password", "email")));
+    }
+
+    @Test
+    public void registerFail() throws DataAccessException {
+        registerService.register(new RegisterRequest("username", "password", "email"));
+        assertThrows(DataAccessException.class, () -> registerService.register(new RegisterRequest("username", "password", "email")));
+    }
+}
