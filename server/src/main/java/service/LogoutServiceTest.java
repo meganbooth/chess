8dc21ee4ebb7d9f203501c5ae32d476568baf682
Megan Moody <meganboothemail@gmail.com>
2026-07-15 17:24:02 -0700

    Phase 3 Unit Tests - implement logout service test

---
+++ server/src/main/java/service/LogoutServiceTest.java
@@ -0,0 +1,37 @@
+package service;
+
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.request.LogoutRequest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogoutServiceTest {
+    private MemoryAuthDAO authDAO;
+    private LogoutService logoutService;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        authDAO = new MemoryAuthDAO();
+        logoutService = new LogoutService(authDAO);
+
+        AuthData auth = new AuthData("authToken", "username");
+        authDAO.createAuth(auth);
+    }
+
+    @Test
+    public void logoutSuccess() {
+        assertDoesNotThrow(() -> logoutService.logout(new LogoutRequest("authToken")));
+    }
+
+    @Test
+    public void logoutFail() {
+        assertThrows(DataAccessException.class, () -> logoutService.logout(new LogoutRequest("wrongAuthToken")));
+    }
+}
