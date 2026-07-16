af75302858b8f6787c9c51714c540ca37df8af91
Megan Moody <meganboothemail@gmail.com>
2026-07-15 17:34:11 -0700

    Phase 3 Unit Tests - implement list games service test

---
+++ server/src/main/java/service/ListGamesServiceTest.java
@@ -0,0 +1,44 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.request.ListGamesRequest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ListGamesServiceTest {
+    private MemoryAuthDAO authDAO;
+    private MemoryGameDAO gameDAO;
+    private ListGamesService listGamesService;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        listGamesService = new ListGamesService(authDAO,gameDAO);
+
+        AuthData auth = new AuthData("authToken", "username");
+        authDAO.createAuth(auth);
+        GameData game = new GameData(1234, "whiteUsername",
+                "blackUsername", "game1", new ChessGame());
+        gameDAO.createGame(game);
+    }
+
+    @Test
+    public void listGamesSuccess() {
+        assertDoesNotThrow(() -> listGamesService.listGames(new ListGamesRequest("authToken")));
+    }
+
+    @Test
+    public void listGamesFail() {
+        assertThrows(DataAccessException.class, () -> listGamesService.listGames(new ListGamesRequest("wrongAuthToken")));
+    }
+}
