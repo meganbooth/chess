5193acc442dc556efc1849b7b8373e57f1f11ba0
Megan Moody <meganboothemail@gmail.com>
2026-07-17 18:52:55 -0700

    Phase 4 GameDAO - implement clear and createGame with tests

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -1,9 +1,14 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
+
+import static dataaccess.DatabaseManager.getConnection;
 
 public class MySqlGameDAO implements GameDAO{
     @Override
@@ -13,12 +18,29 @@ public class MySqlGameDAO implements GameDAO{
 
     @Override
     public void clear() throws DataAccessException {
-        throw new DataAccessException("not implemented");
+        var statement = "DELETE FROM Games";
+        try (var conn = getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to clear games", ex);
+        }
     }
 
     @Override
     public void createGame(GameData game) throws DataAccessException {
-        throw new DataAccessException("not implemented");
+        var statement = "INSERT INTO Games (gameID, whiteUsername, blackUsername, gameName, gameState) VALUES (?, ?, ?, ?, ?)";
+        try (var conn = getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.setInt(1, game.gameID());
+            preparedStatement.setString(2, game.whiteUsername());
+            preparedStatement.setString(3, game.blackUsername());
+            preparedStatement.setString(4, game.gameName());
+            preparedStatement.setString(5, new Gson().toJson(game.game()));
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to insert game", ex);
+        }
     }
 
     @Override
--- server/src/test/java/dataaccess/MySqlGameDAOTest.java
+++ server/src/test/java/dataaccess/MySqlGameDAOTest.java
@@ -1,4 +1,46 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MySqlGameDAOTest {
+    private MySqlUserDAO userDAO;
+    private MySqlAuthDAO authDAO;
+    private MySqlGameDAO gameDAO;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        userDAO = new MySqlUserDAO();
+        authDAO = new MySqlAuthDAO();
+        gameDAO = new MySqlGameDAO();
+        gameDAO.clear();
+        authDAO.clear();
+        userDAO.clear();
+    }
+
+    @Test
+    public void clearSuccess() {
+        assertDoesNotThrow(() -> gameDAO.clear());
+    }
+
+    @Test
+    public void createGameSuccess() throws DataAccessException{
+        assertDoesNotThrow(() -> gameDAO.createGame(new GameData(1234,null,
+                null,"gameName", new ChessGame())));
+    }
+
+    @Test
+    public void createGameFail() throws DataAccessException {
+        assertDoesNotThrow(() -> gameDAO.createGame(new GameData(1234,null,
+                null,"gameName", new ChessGame())));
+        assertThrows(DataAccessException.class, () -> gameDAO.createGame(new GameData(1234,null,
+                null,"gameName", new ChessGame())));
+    }
 }
