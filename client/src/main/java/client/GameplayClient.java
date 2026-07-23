e9046591341061f72f76e0219ac5c8e78ca25f70
Megan Moody <meganboothemail@gmail.com>
2026-07-22 19:28:44 -0700

    Phase 5 Prelogin - login/register set switch flag and authToken

--- client/src/main/java/client/GameplayClient.java
+++ client/src/main/java/client/GameplayClient.java
@@ -1,7 +1,15 @@
 package client;
 
+import java.util.Scanner;
+
 public class GameplayClient implements Client {
+    public ServerFacade facade = new ServerFacade(8080);
+
+    private String authToken = null;
     private boolean quit = false;
+    private boolean switchClient = false;
+
+    Scanner scanner = new Scanner(System.in);
 
     public String handleInput(String input) {
         return "gameplay received: " + input;
@@ -9,5 +17,11 @@ public class GameplayClient implements Client {
 
     public boolean shouldQuit() {
         return quit;
+    }
+    public boolean shouldSwitchClient() {
+        return switchClient;
+    }
+    public String getAuthToken() {
+        return authToken;
     }
 }
--- client/src/main/java/client/PreloginClient.java
+++ client/src/main/java/client/PreloginClient.java
@@ -1,11 +1,19 @@
 package client;
 
+import model.result.LoginResult;
+import model.result.RegisterResult;
+
 import java.util.Scanner;
 
 public class PreloginClient implements Client{
     public ServerFacade facade = new ServerFacade(8080);
+
+    private String authToken = null;
     private boolean quit = false;
+    private boolean switchClient = false;
+
     Scanner scanner = new Scanner(System.in);
+
     public String handleInput(String input) {
         return switch (input) {
             case "help" ->
@@ -28,17 +36,45 @@ public class PreloginClient implements Client{
                 System.out.print("Email: ");
                 String email = scanner.nextLine();
                 try {
-                    facade.register(username,password,email);
+                    RegisterResult result = facade.register(username,password,email);
+                    authToken = result.authToken();
+                    switchClient = true;
                     yield "Account successfully created";
                 } catch(Exception e) {
                     yield "Error: Account not created. That username may already be taken.";
                 }
             }
-            case "login" -> "login";
-            default -> "other";
+            case "login" -> {
+                System.out.print("Username: ");
+                String username = scanner.nextLine();
+                System.out.print("Password: ");
+                String password = scanner.nextLine();
+                try {
+                    LoginResult result = facade.login(username,password);
+                    authToken = result.authToken();
+                    switchClient = true;
+                    yield "Welcome";
+                } catch(Exception e) {
+                    yield "Error: Incorrect username or password.";
+                }
+            }
+            default -> """
+                    Command not recognized.
+                    Available commands:
+                      register - create a new account
+                      login - sign in to an existing account
+                      quit - exit the program
+                      help - show this menu
+                    """;
         };
     }
     public boolean shouldQuit() {
         return quit;
+    }
+    public boolean shouldSwitchClient() {
+        return switchClient;
+    }
+    public String getAuthToken() {
+        return authToken;
     }
 }
