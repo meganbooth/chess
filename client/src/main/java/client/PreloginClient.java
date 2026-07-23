7c21bd9fcd4d9c64f19387b59a570f9f46a94533
Megan Moody <meganboothemail@gmail.com>
2026-07-22 18:52:57 -0700

    Phase 5 Prelogin - register wired with prompts

--- client/src/main/java/client/PreloginClient.java
+++ client/src/main/java/client/PreloginClient.java
@@ -1,7 +1,11 @@
 package client;
 
+import java.util.Scanner;
+
 public class PreloginClient implements Client{
+    public ServerFacade facade = new ServerFacade(8080);
     private boolean quit = false;
+    Scanner scanner = new Scanner(System.in);
     public String handleInput(String input) {
         return switch (input) {
             case "help" ->
@@ -16,7 +20,20 @@ public class PreloginClient implements Client{
                 quit = true;
                 yield "Thanks for playing!";
             }
-            case "register" -> "register";
+            case "register" -> {
+                System.out.print("Username: ");
+                String username = scanner.nextLine();
+                System.out.print("Password: ");
+                String password = scanner.nextLine();
+                System.out.print("Email: ");
+                String email = scanner.nextLine();
+                try {
+                    facade.register(username,password,email);
+                    yield "Account successfully created";
+                } catch(Exception e) {
+                    yield "Error: Account not created. That username may already be taken.";
+                }
+            }
             case "login" -> "login";
             default -> "other";
         };
