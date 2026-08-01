ba9a6b3b617270692929f64c6101464e996dc80c
Megan Moody <meganboothemail@gmail.com>
2026-08-01 09:15:37 -0700

    Phase 6 Client - websocket facade action methods

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -3,11 +3,14 @@ package client;
 import chess.ChessMove;
 import com.google.gson.Gson;
 import jakarta.websocket.*;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
 import websocket.messages.ErrorMessage;
 import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
+import java.io.IOException;
 import java.net.URI;
 
 import static websocket.messages.ServerMessage.ServerMessageType.LOAD_GAME;
@@ -15,12 +18,10 @@ import static websocket.messages.ServerMessage.ServerMessageType.LOAD_GAME;
 @ClientEndpoint
 public class WebSocketFacade {
     private final Session session;
-    private final ServerMessageObserver observer;
 
     public WebSocketFacade(String url, ServerMessageObserver observer) throws Exception {
         url = url.replace("http", "ws");
         URI socketURI = new URI(url + "/ws");
-        this.observer = observer;
 
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         this.session = container.connectToServer(this, socketURI);
@@ -47,20 +48,32 @@ public class WebSocketFacade {
         });
     }
 
-    public void connect(String authToken, int gameID) {
-        // connect
+    public void connect(String authToken, int gameID) throws IOException {
+        UserGameCommand userGameCommand = new UserGameCommand(UserGameCommand.CommandType.CONNECT,authToken,gameID);
+        var serializer =  new Gson();
+        String json = serializer.toJson(userGameCommand);
+        session.getBasicRemote().sendText(json);
     }
 
-    public void makeMove(String authToken, int gameID, ChessMove move) {
-        // makeMove
+    public void makeMove(String authToken, int gameID, ChessMove move) throws IOException {
+        MakeMoveCommand makeMoveCommand = new MakeMoveCommand(authToken,gameID,move);
+        var serializer =  new Gson();
+        String json = serializer.toJson(makeMoveCommand);
+        session.getBasicRemote().sendText(json);
     }
 
-    public void leave(String authToken, int gameID) {
-        // leave
+    public void leave(String authToken, int gameID) throws IOException {
+        UserGameCommand userGameCommand = new UserGameCommand(UserGameCommand.CommandType.LEAVE,authToken,gameID);
+        var serializer =  new Gson();
+        String json = serializer.toJson(userGameCommand);
+        session.getBasicRemote().sendText(json);
     }
 
-    public void resign(String authToken, int gameID) {
-        // resign
+    public void resign(String authToken, int gameID) throws IOException {
+        UserGameCommand userGameCommand = new UserGameCommand(UserGameCommand.CommandType.RESIGN,authToken,gameID);
+        var serializer = new Gson();
+        String json = serializer.toJson(userGameCommand);
+        session.getBasicRemote().sendText(json);
     }
 
 
